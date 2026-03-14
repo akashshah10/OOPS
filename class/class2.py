7d50b3ad8f724ec22c793124440b4183b74d707c
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-03-14 16:52:16 +0530

    create pin

--- class/class2.py
+++ class/class2.py
@@ -1,7 +1,7 @@
 class Atm:
     
     #Constructor
-    
+     
     def __init__(self):
         self.pin = ""
         self.balance = 0
@@ -18,7 +18,7 @@ class Atm:
                            5. Enter 5 to exit
  """)
         if user_input == "1":
-            print("Create pin")
+            self.create_pin()
 
         elif user_input == "2":
             print("Deposit")
@@ -32,5 +32,9 @@ class Atm:
         else:
             print("Bye")
 
+
+    def create_pin(self):
+        self.pin = input("Enter your pin: ")
+        print("Pin set succesfully")
 
 atm = Atm()
