6d413eff73f32b1aa355066be516ab0831807c71
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-03-14 10:33:19 +0530

    Constructor

--- class/class2.py
+++ class/class2.py
@@ -1,2 +1,12 @@
 class Atm:
     
+    #Constructor
+    
+    def __init__(self):
+        self.pin = "1234"
+        self.balance = 0
+    
+        self.menu()
+    
+    def menu(self):
+        pass
