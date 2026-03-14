26a9f2adb2c89e77a39f261e0e59bc6690ddde31
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-03-14 10:36:29 +0530

    Adding menu

--- class/class2.py
+++ class/class2.py
@@ -3,10 +3,17 @@ class Atm:
     #Constructor
     
     def __init__(self):
-        self.pin = "1234"
+        self.pin = ""
         self.balance = 0
     
         self.menu()
     
     def menu(self):
-        pass
+        user_input = input("""
+                        Hello, how would you like to proceed?
+                           1. Enter 1 to create pin
+                           2. Enter 2 to deposit
+                           3. Enter 3 to withdraw
+                           4. Enter 4 to check balance
+                           5. Enter 5 to exit
+ """)
